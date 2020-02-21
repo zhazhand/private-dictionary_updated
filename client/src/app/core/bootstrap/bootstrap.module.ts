6828a578a2cef698af734e8c6dc8239dac196b17
azhaz <kfkrgf@gmail.com>
2020-02-21 19:02:39 +0200

    initial updated)

--- client/src/app/core/bootstrap/bootstrap.module.ts
+++ client/src/app/core/bootstrap/bootstrap.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTooltipModule, NgbNavModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [],
   imports: [
+    NgbNavModule,
+    NgbCollapseModule,
     NgbTooltipModule,
     CommonModule
   ],
   exports: [
+    NgbNavModule,
+    NgbCollapseModule, 
     NgbTooltipModule,
     CommonModule
   ]
